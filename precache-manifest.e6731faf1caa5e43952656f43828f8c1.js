676b724ac834d3bf028b0026c1d481cb2a2d06e9
Radgus <dominguezradgus@gmail.com>
2021-12-08 08:40:17 -0600

    Updates

---
+++ precache-manifest.e6731faf1caa5e43952656f43828f8c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb5e5c35f3072e40ea5c670a55fbdedf",
+    "url": "/yankenpokemon/index.html"
+  },
+  {
+    "revision": "db592cbdbe29d365e750",
+    "url": "/yankenpokemon/static/css/main.a17a3546.chunk.css"
+  },
+  {
+    "revision": "f63cb84eec5916281ec0",
+    "url": "/yankenpokemon/static/js/2.c7d70814.chunk.js"
+  },
+  {
+    "revision": "8f06b9e0c45e6669bf287c6947cdbc57",
+    "url": "/yankenpokemon/static/js/2.c7d70814.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db592cbdbe29d365e750",
+    "url": "/yankenpokemon/static/js/main.8f5d8e14.chunk.js"
+  },
+  {
+    "revision": "a3522eb363692dd4fa9a",
+    "url": "/yankenpokemon/static/js/runtime-main.e3a57ce6.js"
+  },
+  {
+    "revision": "cd9cb612781afe3280be7eabb8ca726b",
+    "url": "/yankenpokemon/static/media/card-icon.cd9cb612.png"
+  }
+]);
